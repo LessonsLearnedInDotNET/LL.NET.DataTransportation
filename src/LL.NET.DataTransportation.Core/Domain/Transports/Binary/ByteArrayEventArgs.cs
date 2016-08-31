a7a246e05425600eea1a4ace8a0bad80a1615268
larrynburris <larry.n.burris@gmail.com>
2016-08-31 09:39:22 -0400

    Add interface for binary data transportation

---
+++ src/LL.NET.DataTransportation.Core/Domain/Transports/Binary/ByteArrayEventArgs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace LL.NET.DataTransportation.Core.Binary
+{
+    /// <summary>
+    /// An event argument that holds an array of bytes
+    /// </summary>
+    public class ByteArrayEventArgs : EventArgs
+    {
+        /// <summary>
+        /// An immutable set of binary data in the form of a <see cref="byte"/> array.
+        /// </summary>
+        public byte[] Data { get; private set; }
+
+        public ByteArrayEventArgs(byte[] data)
+        {
+            Data = data;
+        }
+    }
+}
---
+++ src/LL.NET.DataTransportation.Core/Domain/Transports/Binary/IBinaryDataTransport.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace LL.NET.DataTransportation.Core.Binary
+{
+    /// <summary>
+    /// Defines the interface of a data transport object that sends and 
+    /// receives data in binary format.
+    /// </summary>
+    public interface IBinaryDataTransport : IDataTransport
+    {
+        /// <summary>
+        /// Transports an array of bytes using an established connection.
+        /// </summary>
+        /// <param name="data">An array of <see cref="byte"/>s</param>
+        void Send(byte[] data);
+
+        /// <summary>
+        /// An event that is raised when an array of bytes are received on an 
+        /// established connection.
+        /// </summary>
+        event EventHandler<ByteArrayEventArgs> DataReceived;
+    }
+}
