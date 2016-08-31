6758e6f6c92847bcafecdd8971914500828f71b8
larrynburris <larry.n.burris@gmail.com>
2016-08-31 10:36:02 -0400

    Add interface for text data transportation

---
+++ src/LL.NET.DataTransportation.Core/Transports/Content/Text/ITextDataTransport.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace LL.NET.DataTransportation.Core.Transports.Content.Text
+{
+    /// <summary>
+    /// Defines the interface of a data transport object that sends and 
+    /// receives data in text format.
+    /// </summary>
+    public interface ITextDataTransport
+    {
+        /// <summary>
+        /// Transports a <see cref="string"/> of text using an established connection.
+        /// </summary>
+        /// <param name="data">An array of <see cref="byte"/>s</param>
+        void Send(string data);
+
+        /// <summary>
+        /// An event that is raised when a <see cref="string"/> of text is
+        /// received on an established connection.
+        /// </summary>
+        event EventHandler<StringEventArgs> DataReceived;
+    }
+}
---
+++ src/LL.NET.DataTransportation.Core/Transports/Content/Text/StringEventArgs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace LL.NET.DataTransportation.Core.Transports.Content.Text
+{
+    /// <summary>
+    /// An event argument that holds an <see cref="string"/> of text
+    /// </summary>
+    public class StringEventArgs : EventArgs
+    {
+        /// <summary>
+        /// An immutable <see cref="string"/> of text data.
+        /// </summary>
+        public string Data { get; private set; }
+
+        public StringEventArgs(string data)
+        {
+            Data = data;
+        }
+    }
+}
