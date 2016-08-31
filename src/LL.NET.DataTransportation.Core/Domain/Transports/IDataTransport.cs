90cc517498763409dd0da02313aa3dbc4760bd88
larrynburris <larry.n.burris@gmail.com>
2016-08-31 09:38:46 -0400

    Init DataTransportation solution and define IDataTransport

---
+++ src/LL.NET.DataTransportation.Core/Domain/Transports/IDataTransport.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace LL.NET.DataTransportation.Core
+{
+    /// <summary>
+    /// Defines interface of a data transport object. All data transportation 
+    /// objects will require implementation specific IO objects that should be
+    /// disposed properly on end of use.
+    /// </summary>
+    public interface IDataTransport : IDisposable
+    {
+        /// <summary>
+        /// Establishes a data transportation connection.
+        /// </summary>
+        void Connect();
+
+        /// <summary>
+        /// Closes a data transportation connection.
+        /// </summary>
+        void Disconnect();
+    }
+}
